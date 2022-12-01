98b05ee013c68eacdcf0dcd54e5c9cf88b016827
Gilberto Junior <gagjunior@hotmail.com>
2022-11-30 23:58:12 -0300

    Implement all operations

--- calculator/src/main/java/br/com/gagjunior/calculator/resources/CalculatorController.java
+++ calculator/src/main/java/br/com/gagjunior/calculator/resources/CalculatorController.java
@@ -20,4 +20,58 @@ public class CalculatorController {
         Double sum = Utils.convertToDouble(numberOne) + Utils.convertToDouble(numberTwo);
         return ResponseEntity.ok(sum);
     }
+
+    @GetMapping(value = "/sub/{numberOne}/{numberTwo}")
+    public ResponseEntity<Double> sub(@PathVariable String numberOne, @PathVariable String numberTwo) {
+        if (!Utils.isNumeric(numberOne) || !Utils.isNumeric(numberTwo)) {
+            throw new UnsuportedOperationException("Please, set a numeric value...");
+        }
+        Double sub = Utils.convertToDouble(numberOne) - Utils.convertToDouble(numberTwo);
+        return ResponseEntity.ok(sub);
+    }
+
+    @GetMapping(value = "/mult/{numberOne}/{numberTwo}")
+    public ResponseEntity<Double> mult(@PathVariable String numberOne, @PathVariable String numberTwo) {
+        if (!Utils.isNumeric(numberOne) || !Utils.isNumeric(numberTwo)) {
+            throw new UnsuportedOperationException("Please, set a numeric value...");
+        }
+        Double mult = Utils.convertToDouble(numberOne) * Utils.convertToDouble(numberTwo);
+        return ResponseEntity.ok(mult);
+    }
+
+    @GetMapping(value = "/div/{numberOne}/{numberTwo}")
+    public ResponseEntity<Double> div(@PathVariable String numberOne, @PathVariable String numberTwo) {
+        if (!Utils.isNumeric(numberOne) || !Utils.isNumeric(numberTwo)) {
+            throw new UnsuportedOperationException("Please, set a numeric value...");
+        }
+        if (Utils.convertToDouble(numberOne) <= 0 || Utils.convertToDouble(numberTwo) <= 0) {
+            throw new UnsuportedOperationException("Error: divide by zero");
+        }
+        Double div = Utils.convertToDouble(numberOne) / Utils.convertToDouble(numberTwo);
+        return ResponseEntity.ok(div);
+    }
+
+    @GetMapping(value = "/avg/{numberOne}/{numberTwo}")
+    public ResponseEntity<Double> avg(@PathVariable String numberOne, @PathVariable String numberTwo) {
+        if (!Utils.isNumeric(numberOne) || !Utils.isNumeric(numberTwo)) {
+            throw new UnsuportedOperationException("Please, set a numeric value...");
+        }
+
+        double sum = Utils.convertToDouble(numberOne) + Utils.convertToDouble(numberTwo);
+        if (sum == 0) {
+            throw new UnsuportedOperationException("Enter at least one number greater than zero");
+        }
+
+        double avg = sum / 2;
+        return ResponseEntity.ok(avg);
+    }
+
+    @GetMapping(value = "/sqrt/{numberOne}")
+    public ResponseEntity<Double> avg(@PathVariable String numberOne) {
+        if (!Utils.isNumeric(numberOne)) {
+            throw new UnsuportedOperationException("Please, set a numeric value...");
+        }
+        double sqrt = Math.sqrt(Utils.convertToDouble(numberOne));
+        return ResponseEntity.ok(sqrt);
+    }
 }
