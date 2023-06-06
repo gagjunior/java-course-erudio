3b04abeccc3fd8a11416b6f7df47035cf4e393be
gilberto.junior <gilberto.junior@cbbasfaltos.com.br>
2023-06-06 10:45:11 -0300

    Initial commit cambio-service

---
+++ cambio-service/src/test/java/br/com/gagjunior/cambio/CambioServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gagjunior.cambio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CambioServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
