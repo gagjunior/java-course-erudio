68e6eb9229ba24018f698058052e82f144a6dd8d
gilberto.junior <gilberto.junior@cbbasfaltos.com.br>
2023-06-06 17:31:10 -0300

    implement CambioController

--- cambio-service/src/main/java/br/com/gagjunior/cambio/controllers/CambioController.java
+++ cambio-service/src/main/java/br/com/gagjunior/cambio/controllers/CambioController.java
@@ -1,23 +1,42 @@
 package br.com.gagjunior.cambio.controllers;
 
 import br.com.gagjunior.cambio.models.Cambio;
+
+import br.com.gagjunior.cambio.repositories.CambioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 @RestController
 @RequestMapping("cambio-service")
 public class CambioController {
-
+	
+	@Autowired
+	private Environment environment;
+    @Autowired
+	private CambioRepository cambioRepository;
+	
     @GetMapping(value = "/{amount}/{from}/{to}")
     public Cambio getCambio(
-            @PathVariable("amount") BigDecimal amount,
-            @PathVariable("from") String from,
-            @PathVariable("to") String to
+            @PathVariable BigDecimal amount,
+            @PathVariable String from,
+            @PathVariable String to
     ) {
-        return new Cambio(1L, from, to, BigDecimal.ONE, BigDecimal.ONE, "PORT 8000");
+        Cambio cambio = cambioRepository.findByFromAndTo(from, to);
+        if (cambio == null){
+            throw new RuntimeException("Currency Unsuported");
+        }
+    	String port = environment.getProperty("local.server.port");
+        BigDecimal conversionFactor = cambio.getConversionFactor();
+        BigDecimal convertedValue = conversionFactor.multiply(amount);
+        cambio.setConvertedValue(convertedValue.setScale(2, RoundingMode.CEILING));
+        cambio.setEnvironment(port);
+        return cambio;
     }
 }
---
+++ cambio-service/src/main/java/br/com/gagjunior/cambio/repositories/CambioRepository.java
@@ -0,0 +1,11 @@
+package br.com.gagjunior.cambio.repositories;
+
+import br.com.gagjunior.cambio.models.Cambio;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CambioRepository extends JpaRepository<Cambio, Long> {
+
+    Cambio findByFromAndTo(String from, String to);
+}
