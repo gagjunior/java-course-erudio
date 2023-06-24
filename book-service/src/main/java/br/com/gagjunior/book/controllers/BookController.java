e352cbf2e10f8f2a807cad03c91ebcef8a816b24
Gilberto Junior <gagjunior@hotmail.com>
2023-06-23 23:33:20 -0300

    integrate microservices with feing

--- book-service/src/main/java/br/com/gagjunior/book/controllers/BookController.java
+++ book-service/src/main/java/br/com/gagjunior/book/controllers/BookController.java
@@ -1,7 +1,5 @@
 package br.com.gagjunior.book.controllers;
 
-import java.util.Date;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,30 +8,37 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.gagjunior.book.models.Book;
+import br.com.gagjunior.book.proxy.CambioProxy;
+import br.com.gagjunior.book.response.Cambio;
 import br.com.gagjunior.book.services.BookService;
 
 @RestController
 @RequestMapping("book-service")
 public class BookController {
 
-    @Autowired
-    private Environment environment;
-    
-    @Autowired
-    private BookService bookService;
+	@Autowired
+	private Environment environment;
 
-    @GetMapping(value = "/{id}/{currency}")
-    public Book findBook(
-            @PathVariable("id") Long id,
-            @PathVariable("currency") String currency
-    ) {
-    	Book book = bookService.findBook(id);
-    	if (book == null) {
-    		throw new RuntimeException("Book not Found");			
+	@Autowired
+	private BookService bookService;
+
+	@Autowired
+	private CambioProxy cambioProxy;
+
+	@GetMapping(value = "/{id}/{currency}")
+	public Book findBook(@PathVariable("id") Long id, @PathVariable("currency") String currency) {
+
+		Book book = bookService.findBook(id);
+		if (book == null) {
+			throw new RuntimeException("Book not Found");
 		}
-        String port = environment.getProperty("local.server.port");
-        book.setEnvironment(port);
 
-        return book;
-    }
+		Cambio cambio = cambioProxy.getCambio(book.getPrice(), "USD", currency);
+
+		String port = environment.getProperty("local.server.port");
+		book.setEnvironment(port + "FEING");
+		book.setPrice(cambio.getConvertedValue());
+
+		return book;
+	}
 }
